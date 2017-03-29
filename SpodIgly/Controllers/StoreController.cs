932207a1e4f4aa95f364ff86d74589ea2ceb824b
Józef Dabiri <vellyezz@MacBook-Pro-Jozef.local>
2017-03-29 15:31:13 +0200

    [FEATURE]
    ASP .NET MVC Shop

---
+++ SpodIgly/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace SpodIgly.Controllers
+{
+    public class StoreController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult List(string genername)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ SpodIgly/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SpodIgly.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string IconFilename { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
